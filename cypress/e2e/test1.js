77ba92bbb78d08018802e642dd95d6aea4cddd04
asmaanassar <asmaanassar89@gmail.com>
2023-06-19 00:17:27 +0300

    update

--- cypress/e2e/test1.js
+++ cypress/e2e/test1.js
@@ -1,14 +1,24 @@
-// // beforeEach(() => {
-// //     cy.clearAllCookies();
-// //     cy.clearAllLocalStorage();
-// //     cy.clearAllSessionStorage();
-// //     cy.log('Cache and cookies cleared');
-// //     cy.reload()
-// // });
+///<reference types="Cypress"/>
+describe('testing Automation test store', () => {
+    it('testing1', () => {
+        cy.visit("https://automationteststore.com/")
+        cy.get("a[href='https://automationteststore.com/index.php?rt=content/contact']").click()
+       cy.get('#ContactUsFrm_first_name').type("AsmaNassar")
+       cy.get("input[id='ContactUsFrm_email']").type("asmaanassar89@gmail.com")
+       cy.get("textarea[name='enquiry']").type("this is a test")
+       cy.get("button[title='Submit']").click()
+    });
 
-// // when i use any browser either chrome we dont use the codes to delet cash memory
-// ///<reference types="Cypress"/>
-// describe('log in testing', () => {
-//     it('test for log in-standared user ', () => {
-//         cy.visit("https://www.saucedemo.com/")
+
+    it.only('test2', () => {
+        cy.visit("https://automationteststore.com/")
+        cy.get("a[href='https://automationteststore.com/index.php?rt=content/contact']").click()
+       cy.get('#ContactUsFrm_first_name').type("AsmaNassar")
+       cy.get("input[id='ContactUsFrm_email']").type("asmaanassar89@gmail.com")
+       cy.get("textarea[name='enquiry']").type("this is a test")
+       cy.get("button[title='Submit']").click()
+       cy.get('.mb40 > :nth-child(3)').should("have.text","Your enquiry has been successfully sent to the store owner!")
+        
+    });
+});
         
--- cypress/e2e/test2.js
+++ cypress/e2e/test2.js
@@ -2,8 +2,13 @@
 describe('test css selector', () => {
     it('test1', () => {
         cy.visit("https://automationteststore.com/")
-    cy.get("img[src='//automationteststore.com/image/thumbnails/18/6f/demo_product03_jpg-100094-250x250.jpg']").click()
+        cy.get("img[src='//automationteststore.com/image/thumbnails/18/6f/demo_product03_jpg-100094-250x250.jpg']").click()
+        cy.get(".active.menu_home").click()
+        cy.get("img[src='//automationteststore.com/image/thumbnails/18/6b/demo_product02_3_jpg-100029-250x250.jpg']").click()
         // cy.get("a[title='Flash Bronzer Body Gel']").click()
-  //cy.get("//a[text()='Flash Bronzer Body Gel']")
+        //cy.get("//a[text()='Flash Bronzer Body Gel']")
     });
 });
+
+
+

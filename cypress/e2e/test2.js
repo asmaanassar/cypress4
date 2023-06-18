aa97736f601df2a2091f306c3a82dd02c51278c5
asmaanassar <asmaanassar89@gmail.com>
2023-06-18 19:50:05 +0300

    update

--- cypress/e2e/test2.js
+++ cypress/e2e/test2.js
@@ -2,8 +2,8 @@
 describe('test css selector', () => {
     it('test1', () => {
         cy.visit("https://automationteststore.com/")
-        //cy.get("img[src='//automationteststore.com/image/thumbnails/18/6f/demo_product03_jpg-100094-250x250.jpg']").click()
-        cy.get("a[title='Flash Bronzer Body Gel']").click()
-       //cy.get("//a[text()='Flash Bronzer Body Gel']")
+    cy.get("img[src='//automationteststore.com/image/thumbnails/18/6f/demo_product03_jpg-100094-250x250.jpg']").click()
+        // cy.get("a[title='Flash Bronzer Body Gel']").click()
+  //cy.get("//a[text()='Flash Bronzer Body Gel']")
     });
 });
